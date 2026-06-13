103e34ff2b3f0110a07bd9fedff9500b9e016df4
Nara <narasatish966@gmail.com>
2026-06-13 09:30:51 +0530

    Phase 5: extend modern theme to in-app (accessible focus rings) v251

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 ﻿// LandingPrep service worker â€” offline support.
 // Bump CACHE_VERSION on every deploy so clients pick up new assets.
-const CACHE_VERSION = "lp-v250";
+const CACHE_VERSION = "lp-v251";
 const CORE = [
   "./",
   "./index.html",
