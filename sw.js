85746068ac34e8d8ec2d8353166f8e0cf7626c7a
Nara <narasatish966@gmail.com>
2026-06-25 22:59:03 +0530

    build: reconcile artifacts after rebase (v303)

--- sw.js
+++ sw.js
@@ -1,14 +1,14 @@
 ﻿// LandingPrep service worker â€” offline support.
 // Bump CACHE_VERSION on every deploy so clients pick up new assets.
-const CACHE_VERSION = "lp-v302";
+const CACHE_VERSION = "lp-v303";
 const CORE = [
   "./",
   "./index.html",
   "./theme.css",
   "./manifest.json",
   "./icon.svg",
-  "./vendor/react.production.min.js?v=302",
-  "./vendor/react-dom.production.min.js?v=302",
+  "./vendor/react.production.min.js?v=303",
+  "./vendor/react-dom.production.min.js?v=303",
 ];
 
 self.addEventListener("install", (e) => {
